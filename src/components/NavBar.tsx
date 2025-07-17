1db80a1cdf38df1bdd9ba5cfd450d2ecab7f5ae8
fatima-ca <fatimacastilloag@gmail.com>
2025-07-16 18:03:22 -0600

    cambio imagen en navegador

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,20 +4,33 @@ export default function NavBar() {
   return (
     <header className="bg-pucered p-4 text-blue">
   <nav className="flex flex-col md:flex-row justify-between items-center max-w-6xl mx-auto">
-    <h1 className="text-xl font-bold">Miri's Bday</h1>
+    
     <ul className="flex flex-col md:flex-row gap-4 mt-4 md:mt-0">
+
       <li>
-        <Link to='/' className="hover:text-gray-200 transition">Home</Link>
+        <Link to='/' className="hover:opacity-30 transition">
+          <img src="home.svg" alt="Home" className="h-8 w-auto"/>
+        </Link>
       </li>
+
       <li>
-        <Link to='/playlist' className="hover:text-gray-200 transition">Playlist</Link>
+        <Link to='/playlist' className="hover:opacity-30 transition">
+          <img src="p1.svg" alt="Playlist" className="h-8 w-auto"/>
+        </Link>
       </li>
+      
       <li>
-        <Link to='/remains' className="hover:text-gray-200 transition">Remains</Link>
+        <Link to='/remains' className="hover:opacity-30 transition">
+          <img src="p2.svg" alt="Remains" className="h-8 w-auto"/>
+        </Link>
       </li>
+
       <li>
-        <Link to='/letter' className="hover:text-gray-200 transition">Letter</Link>
+        <Link to='/letter' className="hover:opacity-30 transition">
+          <img src="p3.svg" alt="Remains" className="h-8 w-auto"/>
+        </Link>
       </li>
+
     </ul>
   </nav>
 </header>
